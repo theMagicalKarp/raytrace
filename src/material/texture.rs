fc0015026b507d0432477231d08e0f7d032a81a1
Robert Sheehy <gameboy1092@gmail.com>
2025-03-26 20:49:51 -0500

    Introduce noise/image textures (#13)

--- src/material/texture.rs
+++ src/material/texture.rs
@@ -1,3 +1,6 @@
+use crate::interval::Interval;
+use crate::noise::Perlin;
+use image::RgbImage;
 use nalgebra::Vector3;
 use std::fmt::Debug;
 use std::sync::Arc;
@@ -64,5 +67,60 @@ impl Texture for Checkered {
             0 => self.even.value(u, v, p),
             _ => self.odd.value(u, v, p),
         }
+    }
+}
+
+#[derive(Debug)]
+pub struct Image {
+    pub data: RgbImage,
+}
+
+unsafe impl Sync for Image {}
+unsafe impl Send for Image {}
+
+impl Image {
+    pub fn new(data: RgbImage) -> Self {
+        Image { data }
+    }
+}
+
+impl Texture for Image {
+    fn value(&self, u: f32, v: f32, _: Vector3<f32>) -> Vector3<f32> {
+        let interval = Interval::new(0.0, 1.0);
+
+        let u = interval.clamp(u);
+        let v = interval.clamp(v);
+
+        let i = (u * self.data.width() as f32) as u32;
+        let j = (v * self.data.height() as f32) as u32;
+
+        let pixel = self.data.get_pixel(i, j);
+        let color_scale = 1.0 / 256.0;
+
+        Vector3::new(pixel[0] as f32, pixel[1] as f32, pixel[2] as f32) * color_scale
+    }
+}
+
+#[derive(Debug)]
+pub struct Noise {
+    pub perlin: Perlin,
+    pub scale: f32,
+    pub turbulance: u32,
+}
+
+impl Noise {
+    pub fn new(scale: f32, turbulance: u32) -> Self {
+        let perlin = Perlin::default();
+        Noise {
+            perlin,
+            scale,
+            turbulance,
+        }
+    }
+}
+
+impl Texture for Noise {
+    fn value(&self, _: f32, _: f32, p: Vector3<f32>) -> Vector3<f32> {
+        Vector3::new(1.0f32, 1.0f32, 1.0f32) * (self.perlin.turb(p * self.scale, self.turbulance))
     }
 }
